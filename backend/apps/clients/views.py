10e92ce8c2360014c6b93a31c537b04d5e7c547a
jeffersonamado <jeffersonamado@gmail.com>
2024-11-02 15:43:52 -0500

    feat(client): improve clients apps

--- backend/apps/clients/views.py
+++ backend/apps/clients/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status
+from rest_framework.response import Response
 
 from .models import Customer
 from .serializer import CustomerSerializer
@@ -6,3 +7,16 @@ from .serializer import CustomerSerializer
 class ListCreateClients(generics.ListAPIView):
   queryset = Customer.objects.all()
   serializer_class = CustomerSerializer
+  
+  def post(self, request, *args, **kwargs):
+    data= request.data
+    serr = CustomerSerializer(data)
+    if (serr.is_valid()):
+      serr.save()
+      return Response(serr.validated_data, status=status.HTTP_200_OK)  
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+    
