93271fe6bf39a55d328f0bf055fcf98ada3c960c
japeto <jeffersonamado@gmail.com>
2024-11-12 10:31:52 -0500

    fix(customer): improve model and settings

--- backend/apps/clients/models.py
+++ backend/apps/clients/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
+    customer_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
--- backend/facturion/settings.py
+++ backend/facturion/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'facturion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pos_course',
+        'USER': 'postgres',
+        'PASSWORD': 'aP4sw0rd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
